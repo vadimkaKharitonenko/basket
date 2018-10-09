873f405ef1e882d6617d76e5c83dae74f166b946
vadimkakharitonenko <vadimka.kharitonenko@mail.ru>
2018-10-09 14:30:01 +0300

    Updates

---
+++ precache-manifest.45df94c657eaed18795f45e2a5586350.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "71443e123b6dbe8e154b74aee5909636",
+    "url": "/basket/static/media/product2.71443e12.png"
+  },
+  {
+    "revision": "75e49c18c62abd5ba104",
+    "url": "/basket/static/css/main.a218ba68.chunk.css"
+  },
+  {
+    "revision": "52f04c7937469fe41149",
+    "url": "/basket/static/js/1.52f04c79.chunk.js"
+  },
+  {
+    "revision": "cb15464fca7ec8c376e2",
+    "url": "/basket/static/js/runtime~main.cb15464f.js"
+  },
+  {
+    "revision": "a5b46c3c71c6ecbafa4feb3eebba2efb",
+    "url": "/basket/static/media/product1.a5b46c3c.png"
+  },
+  {
+    "revision": "75e49c18c62abd5ba104",
+    "url": "/basket/static/js/main.75e49c18.chunk.js"
+  },
+  {
+    "revision": "f2b8f79d2c3ba6bed89d1881016d755d",
+    "url": "/basket/static/media/product3.f2b8f79d.png"
+  },
+  {
+    "revision": "2e5803a1d62afda89464a21adc7b696a",
+    "url": "/basket/static/media/product4.2e5803a1.png"
+  },
+  {
+    "revision": "d3c6925050858a6ea84cb47add8c1710",
+    "url": "/basket/static/media/product5.d3c69250.png"
+  },
+  {
+    "revision": "a82bef14486c071e7f57c652262101b5",
+    "url": "/basket/static/media/logo.a82bef14.png"
+  },
+  {
+    "revision": "9b43d583723484083f4380e739658f8b",
+    "url": "/basket/index.html"
+  }
+];
